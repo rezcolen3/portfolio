19524c7ca7e2653012389f806f9ab3d29b6dfc86
RezHackXYZ <rhythmupa@gmail.com>
2025-08-21 16:33:10 +0530

    letss gooo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
